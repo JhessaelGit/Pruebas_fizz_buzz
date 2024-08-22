9d17a42a64940f5f736439f774783bede7af9614
Jhessael Siancas <siancas.jhessael@gmail.com>
2024-08-22 09:25:52 -0400

    prueba de fizz con 3 agregado

---
+++ src/fizzbuzz.js
@@ -0,0 +1,10 @@
+function generarFizzBuzz(numero)
+{
+    if(numero===3)
+    {
+        return "Fizz"
+    }
+    return numero+"";
+}
+
+export default generarFizzBuzz;
---
+++ src/fizzbuzz.spec.js
@@ -0,0 +1,17 @@
+import sumador from "./sumador"
+import generarFizzBuzz from "./fizzbuzz";
+describe("FizzBuzz",()=>{
+    it("Generar el mismo numero si no sigue ninguna regla",()=>{
+        
+        expect(generarFizzBuzz(1)).toEqual("1");
+    });
+    it("Generar el mismo numero si no sigue ninguna regla",()=>{
+        expect(generarFizzBuzz(2)).toEqual("2");
+    });
+    it("Generar Fizz para el numero 3",()=>{
+        expect(generarFizzBuzz(3)).toEqual("Fizz");
+    });
+    it("Generar Fizz para multiplos de 3",()=>{
+        expect(generarFizzBuzz(3)).toEqual("Buzz");
+    });
+})
